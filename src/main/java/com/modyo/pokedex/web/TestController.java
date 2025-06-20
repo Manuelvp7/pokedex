3b023c7eb571913ada2981a4ec57400cbc964c34
Manuel Vilchis <jmanuelvp@outlook.es>
2025-06-20 04:10:43 -0600

    Adding actuator dependency for healthcheck

--- src/main/java/com/modyo/pokedex/web/TestController.java
+++ src/main/java/com/modyo/pokedex/web/TestController.java
@@ -2,17 +2,20 @@ package com.modyo.pokedex.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "/test")
 public class TestController {
 
-  @GetMapping
+  @GetMapping("/test")
   public ResponseEntity<Map<String, String>> test() {
     return ResponseEntity.ok().body(Map.of("Testing", "Up and running"));
+  }
+
+  @GetMapping
+  public ResponseEntity<Map<String, String>> healthcheck() {
+    return ResponseEntity.ok().body(Map.of("Status", "Up"));
   }
 }
